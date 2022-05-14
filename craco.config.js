63dbe4cc33a390ad547beeb9c1419f4beffa87d9
lori@kupotech.com <lori@kupotech.com>
2022-05-14 16:20:14 +0800

    init

---
+++ craco.config.js
@@ -0,0 +1,11 @@
+const port = process.env.BLOCKLET_PORT || process.env.PORT || 3000;
+
+module.exports = {
+  devServer: {
+    port,
+    client: {
+      // If you want to development this blocklet without blocklet-server, you can delete next line, otherwise the hot reload will be failed.
+      webSocketURL: 'wss://0.0.0.0/ws',
+    },
+  },
+};
